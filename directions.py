f9e8c17a886280dfbd19307d24e69ace9dc056d1
Shahzaib Gill <shahzaibgill@gmail.com>
2016-10-20 22:22:03 -0400

    Formatting of directions response to remove tags using regex

--- directions.py
+++ directions.py
@@ -1,4 +1,5 @@
 import os
+import re
 import googlemaps
 from datetime import datetime
 
@@ -8,7 +9,9 @@ def getDirections(fromLocation, toLocation, mode, language):
     directions = ""
     directions_result = gmaps.directions(fromLocation,toLocation,mode=mode, departure_time=datetime.now(), language=language)
     for steps in directions_result[0]['legs'][0]['steps']:
-        step = steps['html_instructions'].replace("<b>","")
-        step = step.replace("</b>", "")
-        directions += step + ". "
+        step = steps['html_instructions']
+        directions += step + " ."
+
+    removeTags = re.compile('<.*?>')
+    directions = re.sub(removeTags, '', directions)
     return directions
