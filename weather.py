10df5bc7eed69642816b90f45657871b7f9811ba
s4ayub <shababayub@gmail.com>
2016-10-31 02:08:36 -0400

    updated requirements

---
+++ weather.py
@@ -0,0 +1,23 @@
+import os
+import pyowm
+
+#pyowm allows you to specify language as well with two character id -> english = en
+
+
+owm = pyowm.OWM('your-API-key', language='insert-language-here')  # You MUST provide a valid API key
+
+# Will it be sunny tomorrow at this time in Milan (Italy) ?
+forecast = owm.daily_forecast("Milan,it")
+tomorrow = pyowm.timeutils.tomorrow()
+forecast.will_be_sunny_at(tomorrow)  # Always True in Italy, right? ;-)
+
+# Search for current weather in London (UK)
+observation = owm.weather_at_place('London,uk')
+w = observation.get_weather()
+print(w)                      # <Weather - reference time=2013-12-18 09:20, 
+                              # status=Clouds>
+
+# Weather details
+w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+w.get_humidity()              # 87
+w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
